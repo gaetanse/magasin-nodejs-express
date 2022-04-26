08dc369c6f265a4ba369aa4851f761f11a08a181
Turpin Yannick <TurpinYa@gmail.com>
2022-04-26 16:07:45 +0200

    crééer client, produit, commande

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.use(express.json())
 
 const data = new Data()
 
-app.get('/clients', (res)=>{
+
+
+app.get('/clients', (req, res)=>{
     res.send(data.afficherListeClients())
 })
 
@@ -13,17 +15,67 @@ app.get('/clients/:id' ,(req,res) => {
     
 })
 
+
+
+
 app.get('/produits/:id' ,(req,res) => {
     
 })
 
-app.get('/commandes', (res)=>{
-    res.send(data.afficherListeCommandes())
+
+
+
+app.get('/commandes', (req, res)=>{
+    res.send(data.afficherListeClients())
 })
 
 app.get('/commandes/:id' ,(req,res) => {
     
 })
+
+
+
+
+app.post('/clients', (req, res) => {
+    const {nom, prenom, telephone} = req.body
+    if(nom != undefined && prenom && telephone != undefined) {
+        data.creerClient(nom, prenom,telephone)
+        res.json({message: "client ajouté"})
+    }
+    else
+        res.json({message: "Merci de compléter les données du client"})
+})
+
+app.post('/produits', (req, res) => {
+    const {titre, prix, stock} = req.body
+    if(titre != undefined && prix && stock != undefined) {
+        data.creerProduit (titre, prix, stock)
+        res.json({message: "produit ajouté"})
+    }
+    else
+        res.json({message: "Merci de compléter les données du produit"})
+})
+
+app.post('/commandes', (req, res) => {
+    const {client, listeProduits} = req.body
+    if(client != undefined && listeProduits != undefined) {
+        data.creerCommande(client, listeProduits)
+        res.json({message: "commande ajouté"})
+    }
+    else
+        res.json({message: "Merci de compléter les données de la commande"})
+})
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(888, () => {
 })
