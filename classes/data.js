a0305c863977f723fd9cdc3098e8c94058b356d0
gaetan <gs99@hotmail.fr>
2022-04-26 14:59:36 +0200

    modif class data

---
+++ classes/data.js
@@ -0,0 +1,29 @@
+export class Data{
+    constructor(){
+        
+    }
+    afficherListeClients(){
+        
+    }
+    afficherClient(id){
+
+    }
+    creerClient(id,nom,prenom,telephone){
+
+    }
+    afficherProduit(id){
+        
+    }
+    creerProduit(id,titre,prix,stock){
+
+    }
+    afficherListeCommandes(){
+
+    }
+    afficherCommande(id){
+        
+    }
+    creerCommande(id,client,listeProduits){
+
+    }
+}
