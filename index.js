4631114ce233aebde9f2e54fe11425ac8dcea704
Turpin Yannick <TurpinYa@gmail.com>
2022-04-26 15:29:23 +0200

    ajout index

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express"
 const app = express()
 
+app.use(express.json())
 
+
+app.get('/clients')
+
+app.get('/clients/:id' ,(req,res) => {
+    res.json = dataService.FindClientsByID(req.params.id)
+} )
 
 
 
